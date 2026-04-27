37d4433f174385bd60080869bcf67eb2b8652306
emilyykchan <64957547+emilyykchan@users.noreply.github.com>
2026-04-27 21:02:39 +0000

    Deploying to gh-pages from @ emilyykchan/emilyykchan.github.io@cafa3af231295a61e58ed64a1371731c851333af 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,26 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-talks",
           title: "Talks",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/talks/";
+          },
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "nav-cv",
           title: "CV",
@@ -39,6 +53,17 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2025/plotly/";
+          
+        },
+      },{id: "post-wimpole-estate",
+        
+          title: "Wimpole Estate",
+        
+        description: "A short photo note from one of our closest National Trust places near home.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/nt-wimpole/";
           
         },
       },{id: "post-a-post-with-image-galleries",
@@ -415,54 +440,17 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-02-26-wcb-accepted/";
-            },},{id: "news-tada-our-work-on-comparing-brain-injury-metrics-across-sports-has-been-accepted-for-presentation-at-isbs-2026-at-loughborough-university",
+            },},{id: "news-i-served-as-a-reviewer-for-wcb-2026",
+          title: 'I served as a reviewer for WCB 2026.',
+          description: "",
+          section: "News",},{id: "news-tada-our-work-on-comparing-brain-injury-metrics-across-sports-has-been-accepted-for-presentation-at-isbs-2026-at-loughborough-university",
           title: ':tada: Our work on comparing brain injury metrics across sports has been accepted...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-contactless-jigsaw-puzzle-game",
+          title: 'Contactless Jigsaw Puzzle Game',
+          description: "Gesture-controlled assistive game for children with autism",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/project_puzzlepieces/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
